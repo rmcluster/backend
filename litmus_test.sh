d07d406e9f7d382f9b46dfcf797cd31673cc9e12
Wen Kai Yiang <wyiang@ucdavis.edu>
2026-05-23 14:46:35 -0700

    make litmus script exit if either server or node exits

--- litmus_test.sh
+++ litmus_test.sh
@@ -20,9 +20,14 @@ trap 'kill -9 0' EXIT
 rm -r litmus
 mkdir -p "$DATA_TMPDIR"
 mkdir -p "$RESULTS_DIR"
+OWN_PID=$$
 
 # spin up server
-</dev/null go run . -gcasdb "$DATA_TMPDIR/gcas.db" -storagedb "$DATA_TMPDIR/storage.db" >"$RESULTS_DIR/server.log" 2>&1 &
+( 
+    </dev/null go run . -gcasdb "$DATA_TMPDIR/gcas.db" -storagedb "$DATA_TMPDIR/storage.db" >"$RESULTS_DIR/server.log" 2>&1
+    echo "Server stopped."
+    kill $OWN_PID
+) &
 
 # wait for server to be ready
 while ! grep "Listening on http://0.0.0.0:4917" "$RESULTS_DIR/server.log" &> /dev/null; do
@@ -30,7 +35,11 @@ while ! grep "Listening on http://0.0.0.0:4917" "$RESULTS_DIR/server.log" &> /de
 done
 
 # spin up a node
-</dev/null go run ./cmd/linux-client/ -id node-1 -cas-path "$DATA_TMPDIR/cas" -tracker localhost:4917 -port 1921 >"$RESULTS_DIR/node.log" -announce-ip 127.0.0.1 2>&1 &
+( 
+    </dev/null go run ./cmd/linux-client/ -id node-1 -cas-path "$DATA_TMPDIR/cas" -tracker localhost:4917 -port 1921 >"$RESULTS_DIR/node.log" -announce-ip 127.0.0.1 2>&1
+    echo "Node stopped."
+    kill $OWN_PID
+) &
 
 # wait for node to register
 while ! grep "New announce from node-1" "$RESULTS_DIR/server.log" &> /dev/null; do
