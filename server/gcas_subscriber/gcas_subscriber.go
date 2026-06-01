c3a74143321294454d671359882df3d52aebba97
Angela Zhou <angelajmzhou@gmail.com>
2026-06-01 12:00:02 -0700

    add device nicknames to DB to attribute files to devices; custom webdav field for devices

--- server/gcas_subscriber/gcas_subscriber.go
+++ server/gcas_subscriber/gcas_subscriber.go
@@ -1,6 +1,10 @@
 package gcassubscriber
 
 import (
+	"context"
+	"log"
+	"strings"
+
 	"github.com/rmcluster/backend/server/gcas"
 	"github.com/rmcluster/backend/tracker"
 )
@@ -15,8 +19,13 @@ type GCASSubscriber struct {
 	gcas gcas.GCAS
 }
 
+type deviceMetadataUpserter interface {
+	UpsertDeviceMetadata(ctx context.Context, nodeID, displayName string) error
+}
+
 // OnNodeAdded implements [tracker.TrackerSubscriber].
 func (g *GCASSubscriber) OnNodeAdded(node tracker.RpcServerInfo) {
+	g.upsertDeviceMetadata(node)
 	if node.StoragePort == 0 {
 		return
 	}
@@ -30,11 +39,29 @@ func (g *GCASSubscriber) OnNodeRemoved(node tracker.RpcServerInfo) {
 
 // OnNodeUpdated implements [tracker.TrackerSubscriber].
 func (g *GCASSubscriber) OnNodeUpdated(node tracker.RpcServerInfo) {
+	g.upsertDeviceMetadata(node)
 	if node.StoragePort == 0 {
 		g.gcas.RemoveNode(node.Id)
 		return
 	}
 	g.gcas.ReplaceNode(gcas.NewRemoteCAS(node.Id, node.Ip, node.StoragePort))
+}
+
+func (g *GCASSubscriber) upsertDeviceMetadata(node tracker.RpcServerInfo) {
+	upserter, ok := g.gcas.(deviceMetadataUpserter)
+	if !ok {
+		return
+	}
+	displayName := strings.TrimSpace(node.Nickname)
+	if displayName == "" {
+		displayName = strings.TrimSpace(node.HardwareModel)
+	}
+	if displayName == "" {
+		displayName = "Unknown device"
+	}
+	if err := upserter.UpsertDeviceMetadata(context.Background(), node.Id, displayName); err != nil {
+		log.Printf("gcas: failed to persist device metadata for %s: %v", node.Id, err)
+	}
 }
 
 var _ tracker.TrackerSubscriber = (*GCASSubscriber)(nil)
